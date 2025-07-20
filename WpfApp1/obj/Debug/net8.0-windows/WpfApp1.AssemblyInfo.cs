e488a69ee9040875d3167b396e81e94aaafc97fd
PreetRajput <rajputpreet72@gmail.com>
2025-07-20 15:11:48 +0530

    commit to check

--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169ffb4a526b75e7032154eaf75ad54b770db5bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ad8a474788e7d4e5df1f79d58309df8e444a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
